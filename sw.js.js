5c7b504698bbd7c691183a72baf85477610f8cf1
CJCreatesPNW <cjcreatespnw@gmail.com>
2026-06-19 18:32:17 -0700

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'first-steps-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Serve from cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
